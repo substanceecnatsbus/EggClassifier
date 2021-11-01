358a6a122b7c1095562bf471a735a9ea3222210e
rowickdoggo <rowickdoggo@outlook.com>
2021-11-01 21:08:29 +0800

    ImageAugmentation: Fixed bin/scripts/image_augmentation.py skipping original images.

--- bin/scripts/image_augmentation.py
+++ bin/scripts/image_augmentation.py
@@ -14,14 +14,14 @@ if __name__ == "__main__":
             file_name = file.split(".")[0]
             with Image.open(file_path) as image:
                 image_ndarray = np.array(image)
-            if not file_name.endswith("_flipped_horizontal"):
+            if not file_name.endswith("_flipped_vertical"):
                 image_flipped_vertical = flip_vertical(image_ndarray)
                 image_flipped_vertical = Image.fromarray(
                     image_flipped_vertical)
                 image_flipped_vertical.save(
                     f"{directory_path}/{file_name}_flipped_vertical.jpg")
             else: print(f"Skipping {file_name}")
-            if file_name.endswith("_flipped_horizontal"):
+            if not file_name.endswith("_flipped_horizontal"):
                 image_flipped_horizontal = flip_horizontal(image_ndarray)
                 image_flipped_horizontal = Image.fromarray(
                     image_flipped_horizontal)
