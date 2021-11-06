9423b7b99ff38d9f1973ac4d6ffbdc432ea16df7
rowickdoggo <rowickdoggo@outlook.com>
2021-11-06 23:36:09 +0800

    Classifier: Restructured classifier into a package. Added HistogramClassifier to classifier.

---
+++ egg_classifier/classifier/__init__.py
@@ -0,0 +1,36 @@
+import numpy as np
+from typing import Tuple
+import tensorflow as tf
+from abc import ABC, abstractmethod
+
+tf.autograph.set_verbosity(3)
+
+
+class Classifier(ABC):
+
+    def __init__(self, model_path: str) -> None:
+        self.__load_model(model_path)
+
+    def __load_model(self, model_path: str) -> None:
+        self.__model = tf.keras.models.load_model(model_path)
+
+    def predict(self, data: np.ndarray) -> list[str]:
+        pass
+
+    @property
+    def model(self) -> tf.keras.Model:
+        return self.__model
+
+    @staticmethod
+    @abstractmethod
+    def load_dataset(dataset_path: str, image_size: Tuple,
+                     batch_size: int = 16, test_split: float = 0.1,
+                     seed: int = 123) -> None:
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def train(image_size: Tuple, dataset_path: str,
+              save_path: str = "", number_of_epochs: int = 100,
+              learning_rate: int = 0.0001, test_split: float = 0.1) -> None:
+        pass
---
+++ egg_classifier/classifier/histogram_classifier.py
@@ -0,0 +1,92 @@
+from typing import Tuple, Any
+import os
+import tensorflow as tf
+from egg_classifier.classifier import Classifier
+from PIL import Image
+import numpy as np
+
+tf.autograph.set_verbosity(3)
+
+
+class HistogramClassifier(Classifier):
+
+    @staticmethod
+    def load_dataset(dataset_path: str, image_size: Tuple,
+                     batch_size: int = 16, test_split: float = 0.1,
+                     seed: int = 123) -> Tuple[Tuple[np.ndarray, np.ndarray],
+                                               Tuple[np.ndarray, np.ndarray],
+                                               list[str]]:
+        if test_split >= 1:
+            raise Exception("test_split must be less than 1")
+
+        data = []
+        labels = []
+        fertile_path = f"{dataset_path}/fertile"
+        for file_name in os.listdir(fertile_path):
+            file = f"{fertile_path}/{file_name}"
+            with Image.open(file) as image:
+                resized_image = image.resize(image_size)
+                histogram = resized_image.histogram()
+            x = np.array(histogram, dtype=np.float)
+            data.append(x)
+            labels.append(0)
+        infertile_path = f"{dataset_path}/infertile"
+        for file_name in os.listdir(infertile_path):
+            file = f"{infertile_path}/{file_name}"
+            with Image.open(file) as image:
+                resized_image = image.resize(image_size)
+                histogram = resized_image.histogram()
+            x = np.array(histogram, dtype=np.float)
+            data.append(x)
+            labels.append(1)
+
+        data_count = len(data)
+        data = np.array(data, dtype=np.float)
+        labels = np.array(labels)
+
+        idx = np.random.permutation(data_count)
+        data, labels = data[idx], labels[idx]
+        train_data_count = int(data_count * (1 - test_split))
+
+        train_data = data[:train_data_count]
+        train_labels = labels[:train_data_count]
+        test_data = data[train_data_count:]
+        test_labels = labels[train_data_count:]
+        classes = ["fertile", "infertile"]
+
+        return ((train_data, train_labels), (test_data, test_labels), classes)
+
+    @staticmethod
+    def train(image_size: Tuple, dataset_path: str,
+              save_path: str = "", number_of_epochs: int = 100,
+              learning_rate: int = 0.0001, test_split: float = 0.1) -> Tuple[tf.keras.Model, Any]:
+        inputs = tf.keras.Input(shape=(768))
+        x = tf.keras.layers.LayerNormalization()(inputs)
+        x = tf.keras.layers.Dense(
+            512, activation="relu", kernel_regularizer=tf.keras.regularizers.L2(0.02))(x)
+        x = tf.keras.layers.Dense(
+            1024, activation="relu", kernel_regularizer=tf.keras.regularizers.L2(0.02))(x)
+        x = tf.keras.layers.Dense(
+            2048, activation="relu", kernel_regularizer=tf.keras.regularizers.L2(0.02))(x)
+        x = tf.keras.layers.Dense(
+            1024, activation="relu", kernel_regularizer=tf.keras.regularizers.L2(0.02))(x)
+        x = tf.keras.layers.Dense(
+            512, activation="relu", kernel_regularizer=tf.keras.regularizers.L2(0.02))(x)
+        x = tf.keras.layers.Dropout(0.4)(x)
+        outputs = tf.keras.layers.Dense(
+            1, activation=tf.keras.activations.sigmoid)(x)
+        model = tf.keras.Model(inputs, outputs)
+
+        (train_data, train_labels), (test_data, test_labels), _ = HistogramClassifier.load_dataset(
+            dataset_path, image_size, test_split=test_split)
+
+        model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=learning_rate),
+                      loss=tf.keras.losses.BinaryCrossentropy(),
+                      metrics=['accuracy'])
+        history = model.fit(x=train_data, y=train_labels,
+                            epochs=number_of_epochs,
+                            validation_data=(test_data, test_labels))
+
+        if save_path != "":
+            model.save(save_path)
+        return (model, history)
